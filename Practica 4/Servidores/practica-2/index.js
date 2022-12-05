bbee72f87308570d06200889252cb17fe42b2413
Juan Mesa <55796160+juanmesa1293@users.noreply.github.com>
2022-12-05 18:23:03 -0500

    Practica 4 error Deta

--- Practica 4/Servidores/practica-2/index.js
+++ Practica 4/Servidores/practica-2/index.js
@@ -5,10 +5,11 @@ const app = express();
 let nest;
 
 app.use(async (req, res) => {
-if (!nest) {
-   nest = express();
-   await createServer(nest);
-}
-return nest(req, res);
+  if (!nest) {
+    nest = express();
+    await createServer(nest);
+  }
+  return nest(req, res);
 });
+
 module.exports = app;
